9da3229e91a3ab6c3757f8263fbd2d651ca19afc
EvoTech <iv_zak@yahoo.com>
2011-12-18 03:48:17 +0200

    backends/email.py - supports html, "user" key for old compatible

--- notification/backends/email.py
+++ notification/backends/email.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.core.mail import send_mail
+from django.contrib.sites.models import Site
 from django.core.urlresolvers import reverse
 from django.db.models.loading import get_app
 from django.template import Context
@@ -7,7 +7,10 @@ from django.template.loader import render_to_string
 from django.utils.translation import ugettext
 from django.core.exceptions import ImproperlyConfigured
 
-from django.contrib.sites.models import Site
+if 'mailer' in settings.INSTALLED_APPS:
+    from mailer import send_mail, send_html_mail
+else:
+    from django.core.mail import send_mail, EmailMultiAlternatives
 
 from notification import backends
 from notification.message import message_to_text
@@ -32,6 +35,7 @@ class EmailBackend(backends.BaseBackend):
         
         # update context with user specific translations
         context = Context({
+            "user": recipient,  # Old compatible
             "recipient": recipient,
             "sender": sender,
             "notice": ugettext(notice_type.display),
@@ -42,9 +46,17 @@ class EmailBackend(backends.BaseBackend):
         
         messages = self.get_formatted_messages((
             "short.txt",
-            "full.txt"
+            "full.txt",
+            "full.html",
         ), notice_type.label, context)
-        
+
+        # Fix me. Is it exists a better way to detect is_html?
+        # Chicking, is it default full.html?
+        if messages['full.html'].strip() != ugettext(notice_type.display).strip():
+            is_html = True
+        else:
+            is_html = False
+
         subject = "".join(render_to_string("notification/email_subject.txt", {
             "message": messages["short.txt"],
         }, context).splitlines())
@@ -52,5 +64,19 @@ class EmailBackend(backends.BaseBackend):
         body = render_to_string("notification/email_body.txt", {
             "message": messages["full.txt"],
         }, context)
+
+        body_html = messages['full.html']
         
-        send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [recipient.email])
+        if not is_html:
+            send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
+                      [recipient.email])
+        else:
+            if 'send_html_mail' in globals():
+                send_html_mail(subject, body, messages['full.html'],
+                               settings.DEFAULT_FROM_EMAIL, [recipient.email])
+            else:
+                msg = EmailMultiAlternatives(subject, body,
+                                             settings.DEFAULT_FROM_EMAIL,
+                                             [recipient.email])
+                msg.attach_alternative(body_html, "text/html")
+                msg.send()
