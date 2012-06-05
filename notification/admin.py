4d142a644bbdd920cd7a3bb1bf279a34deec7787
EvoTech <iv_zak@yahoo.com>
2012-06-05 13:03:59 +0300

    admin

--- notification/admin.py
+++ notification/admin.py
@@ -4,19 +4,31 @@ from notification.models import NoticeType, NoticeSetting, Notice, ObservedItem,
 
 
 class NoticeTypeAdmin(admin.ModelAdmin):
-    list_display = ["label", "display", "description", "default"]
+    list_display = ["label", "display", "description", "default", ]
+    search_fields = ["label", "display", "description", ]
 
 
 class NoticeSettingAdmin(admin.ModelAdmin):
-    list_display = ["id", "user", "notice_type", "medium", "send"]
+    list_display = ["id", "user", "notice_type", "medium", "send", ]
+    list_editable = ["send", ]
+    list_filter = ["notice_type", "medium", "send", ]
+    search_fields = ['user__username', 'user__email', ]
+    raw_id_fields = ["user", ]
 
 
 class NoticeAdmin(admin.ModelAdmin):
-    list_display = ["message", "recipient", "sender", "notice_type", "added", "unseen", "archived"]
+    list_display = ["message", "recipient", "sender", "notice_type",
+                    "added", "unseen", "archived", ]
+    search_fields = ['recipient__username', 'recipient__email', ]
+    raw_id_fields = ["recipient", "sender", ]
 
 
 class ObservedItemAdmin(admin.ModelAdmin):
-    list_display = ["user", "content_type", "object_id", "observed_object", "notice_type", "signal", "added", ]
+    list_display = ["user", "content_type", "object_id", "observed_object",
+                    "notice_type", "signal", "added", ]
+    list_filter = ["content_type", "notice_type", "signal", ]
+    search_fields = ['user__username', 'user__email', ]
+    raw_id_fields = ["user", ]
 
 admin.site.register(NoticeQueueBatch)
 admin.site.register(NoticeType, NoticeTypeAdmin)
