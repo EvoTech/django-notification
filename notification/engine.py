f2e2cb1ed98a86551dd5e8d62b7346f6fdbae8cb
EvoTech <ivzak@yandex.ru>
2013-10-15 22:32:02 +0300

    minor ffix

--- notification/engine.py
+++ notification/engine.py
@@ -65,7 +65,7 @@ def send_all(workers=1, processes=False):
     try:
         # nesting the try statement to be Python 2.4
         try:
-            for queued_batch in NoticeQueueBatch.objects.all():
+            for queued_batch in NoticeQueueBatch.objects.order_by("pk"):
                 notices = pickle.loads(str(queued_batch.pickled_data).decode("base64"))
                 for users, label, extra_context, on_site, sender in notices:
 
